0d044fb39b3f71f936d0b4aebb5e0d6d1bbec7fa
Vovek.rao.vivek@gmail.com Vivek.rao.vivek <vovek.rao.vivekgmail.comvivek.rao.vivek@Vovekraovivekgmailcoms-MacBook-Pro.local>
2026-06-24 18:26:59 -0400

    Force fresh clone in startup script

--- startup.sh
+++ startup.sh
@@ -2,20 +2,9 @@
 
 cd /home/site/wwwroot
 
-# Clone or pull latest code
-if [ -d "PythonServerExample" ]; then
-    cd PythonServerExample
-    git pull
-else
-    git clone https://github.com/VraoNOVA/PythonServerExample.git
-    cd PythonServerExample
-fi
-
-# Install dependencies
-pip install django
-
-# Run migrations
-python -m django migrate --settings=settings
+# Remove old copy and clone fresh
+rm -rf PythonServerExample
+git clone https://github.com/VraoNOVA/PythonServerExample.git
 
 # Start the server
-python -m django runserver 0.0.0.0:8000 --settings=settings
+cd /home/site/wwwroot/PythonServerExample && python -m django runserver 0.0.0.0:8000 --settings=settings
