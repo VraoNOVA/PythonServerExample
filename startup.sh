4798ace8ec49a1cbec6f87a8c4eb6c31a2e47018
Vovek.rao.vivek@gmail.com Vivek.rao.vivek <vovek.rao.vivekgmail.comvivek.rao.vivek@Vovekraovivekgmailcoms-MacBook-Pro.local>
2026-06-24 18:32:41 -0400

    Fix startup script - clone to temp dir to avoid self-delete

--- startup.sh
+++ startup.sh
@@ -3,8 +3,10 @@
 cd /home/site/wwwroot
 
 # Remove old copy and clone fresh
+rm -rf PythonServerExample_tmp
+git clone https://github.com/VraoNOVA/PythonServerExample.git PythonServerExample_tmp
 rm -rf PythonServerExample
-git clone https://github.com/VraoNOVA/PythonServerExample.git
+mv PythonServerExample_tmp PythonServerExample
 
 # Start the server
 cd /home/site/wwwroot/PythonServerExample && python -m django runserver 0.0.0.0:8000 --settings=settings
